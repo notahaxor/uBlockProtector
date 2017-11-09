caf405d955bad8041a20a7742f03673ce1b24174
notahaxor <33502756+notahaxor@users.noreply.github.com>
2017-11-08 19:01:36 -0600

    Added 1 new site
    Hopefully, this works,  should help me and others out if it does.  I essentially just added one new site called coin.mg to the list.

--- Extension Compiler/Extension/content/3-content-rules-1-common.js
+++ Extension Compiler/Extension/content/3-content-rules-1-common.js
@@ -102,7 +102,7 @@ if (a.domCmp(["vidoza.net", "videowood.tv", "l2s.io", "adshort.co", "linksh.top"
     "wiziwig.to", "wiziwig.tv", "myp2p.biz", "myp2p.tv", "myp2p.la", "myp2p.ec", "myp2p.eu", "myp2p.sx",
     "myp2p.ws", "myp2p.com", "atdhe.ru", "atdhe.se", "atdhe.bz", "atdhe.top", "u2s.io", "linclik.com",
     "iiv.pl", "linkkawy.com", "zlshorte.net", "ur.ly", "oload.stream", "sawlive.tv", "arenavision.in",
-    "urle.co", "oload.info", "shink.me", "doramasflv.net", "backin.net", "adshort.im"])) {
+    "urle.co", "oload.info", "shink.me", "doramasflv.net", "backin.net", "adshort.im" "coin.mg"])) {
     a.filter("open");
 }
 if (a.domCmp(["drivearabia.com", "putlocker.com", "doatoolsita.altervista.org", "sockshare.com",
@@ -124,7 +124,7 @@ if (a.domCmp(["apkmirror.com", "freepdf-books.com", "bc.vc", "themeslide.com", "
     "leenlink.com", "linkkawy.com", "cut-egy.ml", "igram.im", "adlinkme.com", "oke.io", "wolink.in",
     "bilink.xyz", "git.tc", "zlshorte.net", "shink.me", "oload.stream", "egy-links.com", "infinityurl.co",
     "adsurl.xyz", "firstonetv.net", "lkcash.net", "123link.io", "curs.io", "psl.io", "teqani-plus.com",
-    "link-money.com", "meulink.tk", "mlink.club", "shink.xyz", "tui.click", "adshort.im", "123link.press"])) {
+    "link-money.com", "meulink.tk", "mlink.club", "shink.xyz", "tui.click", "adshort.im", "123link.press", "coin.mg"])) {
     a.timewarp("setInterval", a.matchMethod.stringExact, "1000");
 }
 if (a.domCmp(["katfile.com", "goldescargas.com", "globalbesthosting.com", "userupload.net",
